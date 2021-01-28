8b2e5e5c7f633a9eae7a4834e4f468478831b7a6
crocodile3 <yrt333333@163.com>
2021-01-28 16:48:51 +0800

    后台管理页面搭建

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,25 +1,16 @@
-from flask import Blueprint
-from flask import render_template
+# -*- coding: utf-8 -*-
+from flask import Blueprint,render_template
 
-route_user = Blueprint('user_page', __name__)
+route_user = Blueprint( 'user_page',__name__ )
 
-
-@route_user.route("/login")
+@route_user.route( "/login" )
 def login():
     return render_template( "user/login.html" )
 
-
-@route_user.route("/list")
-def list():
-    return "user list page"
-
-
-route_user.route("/edit")
+@route_user.route( "/edit" )
 def edit():
-    return "user info edit"
+    return render_template( "user/edit.html" )
 
-
-route_user.route("register")
-def register():
-    return "user register"
-
+@route_user.route( "/reset-pwd" )
+def resetPwd():
+    return render_template( "user/reset_pwd.html" )
